304254ea956b4d530a6955935b30c345289a4c9b
Peyman Peirovifar <peirovifar@gmail.com>
2022-05-16 14:56:22 +0200

    Add project files.

---
+++ Övninger2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Övninger2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                ShowMainMeny();
+                string input= Console.ReadLine();
+                switch (input)
+                {
+                    case "0":
+                        Console.WriteLine("felaktig input");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+            while (true);
+            Console.WriteLine("Hello World!");
+        }
+        private static void ShowMainMeny()
+        {
+            Console.WriteLine("0: Ungdom eller pensionär ");
+           
+        }
+    }
+}
